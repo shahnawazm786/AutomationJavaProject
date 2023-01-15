71b4609acaea5793e95609426433546086267535
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-15 14:03:12 +0530

    intial

---
+++ src/demo/FirstExampleClass.java
@@ -0,0 +1,77 @@
+package demo;
+
+class FirstExampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 100 // 200
+		// 2 value (100,200)
+		// operaiton - addition
+		// to show result
+		// variable - the container for the value
+		// value can be - number, words, sentence, logical value, sounds, video,files, database
+		
+		// keyword - 62 which can be used for the container
+		// class, new, public, protected, private, static, final, finally, try, catch, 
+		// if, switch, case,default,break,continue, for, while, do, void, int, long, double,
+		// float, String, main, 
+		// Don't   for the varables
+		// use the special key ($,#,@ etc)
+		// use the space
+		// use the keys of java
+		// Start variable name with number
+		// Does for the variable
+		// variable start with alphabet
+		// follwed number
+		// you can combine more than one word using under score (_)
+		
+		// Number - int, float, long, double
+		// int - whole number (it will not take .) (2 bytes)
+		// float - 4 bytes
+		// long  - int - 8 bytes
+		// double - float - 16 bytes
+		// Words 
+		// char and String
+		// boolean 
+		// int, float, double, long, char, boolean - primitive data type
+		// String, Integer, Float, Double, Long, Boolean - object of primitive data type
+		
+		int a=100; // semi colon represents termination of line 
+		int b=200;
+		int c=a+b; // operator - =, +,-,*,%
+		
+		// category of operator  
+		// Assignment Operator (=)
+			// Pre and Post Assignment 
+		// Arithmetic Operator (+,-,*,/,%)
+		// Conditional Opreator ( >,<,>=,<=,==,!=)
+		// Boolean Operator ( and, or, not)
+		// Special Opretor - Shift Operator >>>,<<<.>>=,<<=
+		
+		// -, + , *, /
+		// reminder %
+		// 7/2 - 3.5 
+		// (3+1/2) - 1 reminder
+		// To display the output/ result/ value, we are using following method
+		// System.out.println();
+		System.out.println(c);
+		System.out.println("Sum of value a and b is ->"+c);
+		System.out.println("Sum of "+a+" and "+b+ " is => " +c);
+		c=a-b;
+		System.out.println("Difference of value a and b is ->"+c);
+		//long a1=400;
+		//long b1=600;
+		c=a*b;
+		System.out.println("Multiplication of value a and b is ->"+c);
+		c=a/b;
+		System.out.println("Division of value a and b is ->"+c);
+		
+		a=10; // reminder 2
+		b=4;
+		c=a%b;
+		System.out.println("Reminder is ->"+c);
+		
+				
+	}
+
+}
