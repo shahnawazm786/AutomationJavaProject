b01425167d1a73ae31a1f58ef656fca8aa96d904
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-21 22:04:19 +0530

    loop

---
+++ src/demo/ForLoopExample.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int num=1; num<=5; num++) {
+			System.out.println("Coding Maktab");
+		}
+		for(int num=1; num<=5; num++) {
+			for(int j=1;j<=5;j++) {
+			System.out.print("  #CodingMaktab");
+		}
+			System.out.println();
+		}
+
+	}
+
+}
---
+++ src/demo/ShowNumberExample.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class ShowNumberExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int odd=1; odd<=100; odd+=2) {
+				System.out.print(odd+"|\t|");
+			}
+			System.out.println();
+			System.out.println("Even Number");
+			for(int even=2; even<=100; even+=2) {
+				System.out.print(even+"|\t|");
+			}
+			
+	}
+
+}
