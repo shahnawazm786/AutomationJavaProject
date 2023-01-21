1fcb754b8dd1423639f4f5b947509599af3447e1
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-21 22:05:16 +0530

    loop

--- src/demo/DoWhileLoopExample.java
+++ src/demo/DoWhileLoopExample.java
@@ -4,7 +4,11 @@ public class DoWhileLoopExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int number=1;
+		do {
+			System.out.println(number);
+			number++;
+		}while(number<=10);
 	}
 
 }
