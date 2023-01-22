7735cf2e6a4f5fffa72fe1533790bdb224476767
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-22 21:45:40 +0530

    22 jan 23

--- src/constructor/ConstructorImplementation.java
+++ src/constructor/ConstructorImplementation.java
@@ -8,6 +8,11 @@ public class ConstructorImplementation {
 		ConstructorExample ce=new ConstructorExample("Java");
 		//System.out.println(ce);
 		ConstructorExample ce1=new ConstructorExample("Java",24);
+		Constructor_01 c1=new Constructor_01();
+		System.out.println(c1.a);
+		System.out.println(c1.name);
+		System.out.println(c1.fees);
+		
 	}
 
 }
