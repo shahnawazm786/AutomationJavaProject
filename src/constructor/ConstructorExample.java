0488512063530f78384f55ff06d2a93e9028f4ac
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-22 21:44:03 +0530

    22 Jan 2023

--- src/constructor/ConstructorExample.java
+++ src/constructor/ConstructorExample.java
@@ -2,4 +2,16 @@ package constructor;
 
 public class ConstructorExample {
 
+	
+	public ConstructorExample() {
+	System.out.println("Constructor Created");	
+	}
+	public ConstructorExample(String name) {
+		System.out.println("My name is "+name);
+	}
+	public ConstructorExample(String name,int hour) {
+		System.out.println("My name is "+name + " total hour is "+hour);
+	}
+	
 }
+
--- src/constructor/ConstructorImplementation.java
+++ src/constructor/ConstructorImplementation.java
@@ -4,8 +4,10 @@ public class ConstructorImplementation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ConstructorExample ce=new ConstructorExample();
-		System.out.println(ce);
+		ConstructorExample c=new ConstructorExample();
+		ConstructorExample ce=new ConstructorExample("Java");
+		//System.out.println(ce);
+		ConstructorExample ce1=new ConstructorExample("Java",24);
 	}
 
 }
