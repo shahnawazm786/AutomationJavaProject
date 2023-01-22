1e5aa034bfffd3ea85c1e827f35ba93045f3068c
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-22 21:38:00 +0530

    22 jan 2023

--- src/constructor/ConstructorImplementation.java
+++ src/constructor/ConstructorImplementation.java
@@ -4,7 +4,8 @@ public class ConstructorImplementation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ConstructorExample ce=new ConstructorExample();
+		System.out.println(ce);
 	}
 
 }
