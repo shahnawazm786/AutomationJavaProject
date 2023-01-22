843d4d93c303065d70f3fc826d58fe2997bf1396
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-01-22 22:03:46 +0530

    poly

--- src/polymorphism/ImplePolymorph.java
+++ src/polymorphism/ImplePolymorph.java
@@ -4,6 +4,8 @@ public class ImplePolymorph {
 	public static void main(String[] args) {
 		PolymorphismExample pe=new PolymorphismExample();
 		System.out.println(String.valueOf(pe.add(100, 200)));
+		System.out.println(String.valueOf(pe.add(100, 20.67f)));
+		System.out.println(String.valueOf(pe.add(100.89f, 20.67f)));
 	}
 
 }
