2abb130eacba227e6b4c24bbed6be07b99239e79
Mohammad Shahnawaz <msnazia2012@gmail.com>
2023-02-23 22:22:06 +0530

    feb

--- src/filesoperations/FileOperationExamples.java
+++ src/filesoperations/FileOperationExamples.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class FileOperationExamples {
 
 	public static void main(String[] args) {
-		//writingStringIntoFile();
+		writingStringIntoFile();
 		readingStringFromFile();
 	}
 	public static void writingStringIntoFile() {
